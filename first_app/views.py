ededd5b0a736bcb5db2b1fd6b6ac069ee5d45207
Shohagh019 <shohaghmunna@gmail.com>
2024-11-04 17:49:03 +0600

    first_commit

---
+++ first_app/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+def contact(request):
+    d = {
+    "name": "Alice",
+    "id": 101,
+    "age": 21,
+    "marks": [
+        {"subject": "Math", "score": 85},
+        {"subject": "Science", "score": 90},
+        {"subject": "English", "score": 88}
+    ]
+}
+    return render(request, 'first_app/contact.html',d)
+def about(request):
+    data = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  }]
+    return render(request, 'first_app/about.html', {'data': data})
